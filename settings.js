2e609df6d3d5e7ad2384869ed5e8b7f256ff116d
Michael Heiniger <michael.heiniger@nzz.ch>
2013-12-03 17:19:30 +0100

    removed instance of settings

--- settings.js
+++
@@ -1,7 +0,0 @@
-var settings = {};
-
-settings.numLEDs = 32;
-settings.stripeType = "LPD8806"; // choose between "LPD8806" and "WS2801"
-settings.spiDevice = "/dev/spidev0.0";
-
-module.exports = settings;
